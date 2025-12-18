578d703297df05b6ab9502101898e0d5bcab4ab6
nisha-sya <netramayathapamagar93@gmail.com>
2025-12-18 13:14:57 +0545

    Add .NET Workshop 8 project files

---
+++ Workshop8/Components/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Workshop8.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Likes { get; set; }
+    }
+}
---
+++ Workshop8/Components/Pages/Calculator.razor.cs
@@ -0,0 +1,14 @@
+﻿namespace Workshop8.Pages
+{
+    public partial class Calculator
+    {
+        double num1;
+        double num2;
+        double result;
+
+        void Add() => result = num1 + num2;
+        void Subtract() => result = num1 - num2;
+        void Multiply() => result = num1 * num2;
+        void Divide() => result = num2 != 0 ? num1 / num2 : 0;
+    }
+}
